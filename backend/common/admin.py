72e2ef2c172a45661c0d2978dcb93403441b2814
Tzourhs <jimaras20111@gmail.com>
2021-01-03 19:50:42 +0200

    On branch tzourhs_models  Your branch is up to date with 'origin/tzourhs_models'.
     Changes to be committed:
    	modified:   backend/backend/settings.py
    	modified:   backend/common/admin.py
    	modified:   backend/common/models.py

--- backend/common/admin.py
+++ backend/common/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Location, Cluster, ChargingPoint, Provider , ChargingStation
 
-# Register your models here.
+admin.site.register(Location)
+admin.site.register(ChargingPoint)
+admin.site.register(Provider)
+admin.site.register(ChargingStation)
+admin.site.register(Cluster)
--- backend/common/models.py
+++ backend/common/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+#uncomment when ready
+#from ... import Owner
 
-# Create your models here.
+
+class Location(models.Model):
+	LocationId = models.AutoField(primary_key=True)
+	#ChargingPoint = models.ForeignKey(ChargingPoint, on_delete = models.CASCADE)
+	email = models.CharField(max_length = 100)
+	website = models.CharField(max_length = 100)
+	telephone = models.CharField(max_length = 13)
+	title = models.CharField(max_length = 100)
+	town = models.CharField(max_length = 100)
+	country = models.CharField(max_length = 100)
+	PostalCode = models.IntegerField()
+	AddresLine = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return f'Id = {self.LocationId}'
+
+class Cluster(models.Model):
+	ClusterName = models.CharField(max_length = 100, default = "Tzourhs")
+	clusterId = models.AutoField(primary_key = True)
+
+	def __str__(self):
+		return f'Id = {self.ClusterId}'
+
+class Provider(models.Model):
+	ProviderId = models.AutoField(primary_key = True)
+	ProviderName = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return f'Id = {self.ProviderId}'
+
+class ChargingStation(models.Model):
+	NrChargingCategories = models.IntegerField()
+	chargingStationId = models.AutoField(primary_key = True)
+	Cluster = models.ForeignKey(Cluster, on_delete = models.CASCADE)
+	Provider = models.ForeignKey(Provider , on_delete = models.CASCADE)
+    
+	def __str__(self):
+		return f'Id = {self.ChargingStationId}'
+
+
+class ChargingPoint(models.Model):
+	#uncomment the next line when ready
+    #owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    #Test
+    owner = models.CharField(max_length = 100 , default = "Tzourhs")
+    #EndTest
+    chargingStation = models.ForeignKey(ChargingStation , on_delete = models.CASCADE, null = True)
+    ChargingPointId = models.AutoField(primary_key=True)
+    location = models.ForeignKey(Location , on_delete = models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    UsageTypeId = models.CharField(max_length = 100)
+    Type = models.CharField(max_length = 100)
+    UsageCost = models.FloatField()
+    KwPower = models.FloatField()
+    VoltsPower = models.FloatField()
+    AmpsPower = models.FloatField()
+
+    def __str__(self):
+        return f'Id = {self.ChargingPointId}'
