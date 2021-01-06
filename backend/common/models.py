180fa02736999707d82b60378a52483f73e0bd92
ilias <il.papandreou@gmail.com>
2021-01-06 19:44:56 +0200

    first set of changes

--- backend/common/models.py
+++ backend/common/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+import uuid
+#class order
 class Payment(models.Model):
-    _paymentMethods = [
+    _PAYMENT_METHODS = [
         ('credit_card','credit card'),
         ('cash','cash'),
         ('paypal','paypal'),
@@ -11,7 +12,7 @@ class Payment(models.Model):
     payment_req = models.BooleanField(default=False)
     payment_method = models.CharField(
         max_length = 20,
-        choices = _paymentMethods, 
+        choices = _PAYMENT_METHODS, 
         default = 'cash'
     )
     cost = models.FloatField(blank = True)
@@ -20,23 +21,23 @@ class Payment(models.Model):
     session_id = models.OneToOneField(Session,on_delete=models.CASCADE)
 
     def __str__(self):
-        return (self.session_id, self.user_id)
+        return str((self.session_id, self.user_id))
 
 
 class Session(models.Model):
-    id = models.CharField(max_lenght=100)
+    id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     user_comments_ratings = models.TextField()
-    cluster_id = models.CharField(max_length=100)   #potential fk
-    kwh_delivered = models.IntegerField()
-    site_id = models.CharField(max_lenth=100)
-    connect_time = models.DateTimeField()
-    disconnect_time = models.DateTimeField()
-    done_charging_time = models.DateTimeField()
-    charging_point_id = models.ForeignKey(ChargingPoint, on_delete=models.SET_NULL)
-    vehicle_id = models.ForeignKey(Vehicle, on_delete=models.SET_NULL)
+    #cluster = models.CharField(max_length=100)   #potential fk Null
+    kwh_delivered = models.IntegerField() #check type
+    site_id = models.UUIDField(editable=False,default=uuid.uuid4)
+    connect_time = models.DateTimeField(null = True)
+    disconnect_time = models.DateTimeField(null = True)
+    done_charging_time = models.DateTimeField(null = True)
+    charging_point = models.ForeignKey(ChargingPoint, on_delete=models.SET_NULL,related_name="sessions")
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.SET_NULL,related_name="sessions")
 
     def __str__(self):
-        return (self.vehicle_id, self.charging_point_id,self.connect_time,self.kwh_delivered)
+        return str((self.vehicle_id, self.charging_point_id,self.connect_time,self.kwh_delivered))
 
 
 
