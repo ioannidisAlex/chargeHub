adbe52cf953ea5e6626eab362f9109e5c187a1e7
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2021-01-14 19:56:24 +0200

    fix provider string method

--- backend/common/models.py
+++ backend/common/models.py
@@ -117,7 +117,7 @@ class Provider(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)	
 
 	def __str__(self):
-		return f'Provider name = {self.provider_name}'
+		return f'Provider name = {self.user.username}'
 
 class ChargingStation(models.Model):
 	#charging_station_id = models.AutoField(primary_key = True)
