8f2a57cf7a188e569dbb22dfb02dfe36912e1bd0
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2021-01-15 14:19:55 +0200

    restructure rest views into folder

--- backend/ev_charging_api/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-
-# Create your views here.
-
-def home(request):
-    return render(request, 'ev_charging_api/api.html')
