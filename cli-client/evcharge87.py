6a2ce4e6d8195c3d8f00f848236d14d9748a5067
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2021-01-17 13:20:18 +0200

    CLI create data points

--- cli-client/evcharge87.py
+++ cli-client/evcharge87.py
@@ -15,7 +15,7 @@ options = {
     "source": click.option("--source", required=True, type=click.File("r")),
     "ev": click.option("--ev", required=True),
     "datefrom": click.option("--datefrom", required=True),
-    "datato": click.option("--datato", required=True),
+    "dateto": click.option("--dateto", required=True),
     "users": click.option("--users", is_flag=True),
     "usermod": click.option("--usermod", is_flag=True),
     "sessionupd": click.option("--sessionupd", is_flag=True),
@@ -23,7 +23,8 @@ options = {
     "resetsessions": click.option("--resetsessions", is_flag=True),
 }
 
-BASE_URL="http://localhost:8765/evcharge/api"
+BASE_URL = "http://localhost:8765/evcharge/api"
+
 
 def apply_options(f):
     for k in reversed(inspect.signature(f).parameters.keys()):
@@ -38,7 +39,6 @@ def show_data(response: requests.Response, *arg, **kwargs):
         response.raise_for_status()
 
 
-
 def store_token(response: requests.Response, *arg, **kwargs):
     if response.status_code == 200:
         with open("softeng20API.token", "w") as f:
@@ -51,22 +51,56 @@ def convert_to_request(f):
     @functools.wraps(f)
     def function(**kw):
         method, url, parameters, hook = f(**kw)
-        return method(f"{BASE_URL}/{url}", hooks=dict(response=hook), timeout=2,**parameters)
+        return method(f"{BASE_URL}/{url}", hooks=dict(response=hook), timeout=2, **parameters)
     return function
+
 
 def convert_to_command(target):
     def inner(f):
         f = convert_to_request(f)
-        f=apply_options(f)
-        return target.command()(f)
+        f = apply_options(f)
+        return target.command()(f)  
     return inner
+
 
 interface = click.Group()
 
 
 @convert_to_command(interface)
 def healthcheck():
-    return requests.get,"healthcheck",{},show_data
+    return requests.get, "healthcheck", {}, show_data
+
+
+# Data endpoints
+
+@convert_to_command(interface)
+def SessionsPerEv(ev, datefrom, dateto, format,apikey):
+    return (
+        requests.get, f"{SessionsPerEv}/{ev}/{datefrom}/{dateto}", 
+        {"payload": dict(format=format)}, show_data
+    )
+
+@convert_to_command(interface)
+def SessionsPerStation(station, datefrom, dateto, format,apikey):
+    return (
+        requests.get, f"{SessionsPerStation}/{station}/{datefrom}/{dateto}", 
+        {"payload": dict(format=format)}, show_data
+    )
+
+@convert_to_command(interface)
+def SessionsPerPoint(point, datefrom, dateto, format,apikey):
+    return (
+        requests.get, f"{SessionsPerPoint}/{point}/{datefrom}/{dateto}", 
+        {"payload": dict(format=format)}, show_data
+    )
+
+@convert_to_command(interface)
+def SessionsPerProvider(provider, datefrom, dateto, format,apikey):
+    return (
+        requests.get, f"{SessionsPerProvider}/{provider}/{datefrom}/{dateto}", 
+        {"payload": dict(format=format)}, show_data
+    )
+
 
 
 if __name__ == "__main__":
