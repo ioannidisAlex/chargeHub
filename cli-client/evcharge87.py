32ee18a76c95f36616bec819397e4c663afb3d74
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2021-01-22 13:38:52 +0200

    administrative endpoints up and running

--- cli-client/evcharge87.py
+++ cli-client/evcharge87.py
@@ -3,6 +3,8 @@ import click
 import inspect
 import requests
 
+from click_option_group import optgroup, RequiredMutuallyExclusiveOptionGroup,AllOptionGroup
+
 
 options = {
     "format": click.option("--format", required=True, type=click.Choice(["json", "csv"])),
@@ -16,9 +18,9 @@ options = {
     "ev": click.option("--ev", required=True,type= click.UUID),
     "datefrom": click.option("--datefrom", required=True,type= click.DateTime(["%Y%m%d"])),
     "dateto": click.option("--dateto", required=True,type= click.DateTime(["%Y%m%d"])),
-    "users": click.option("--users", is_flag=True),
+    "users": click.option("--users", ),
     "usermod": click.option("--usermod", is_flag=True),
-    "sessionupd": click.option("--sessionupd", is_flag=True),
+    "sessionsupd": click.option("--sessionsupd", is_flag=True),
     "healthcheck": click.option("--healthcheck", is_flag=True),
     "resetsessions": click.option("--resetsessions", is_flag=True),
 }
@@ -26,25 +28,7 @@ options = {
 BASE_URL = "http://localhost:8765/evcharge/api"
 
 
-def apply_options(f):
-    for k in reversed(inspect.signature(f).parameters.keys()):
-        f = options[k](f)
-    return f
 
-
-def show_data(response: requests.Response, *arg, **kwargs):
-    if response.status_code == 200:
-        click.echo(response.text)
-    else:
-        response.raise_for_status()
-
-
-def store_token(response: requests.Response, *arg, **kwargs):
-    if response.status_code == 200:
-        with open("softeng20API.token", "w") as f:
-            f.write(response.json()["token"])
-    else:
-        response.raise_for_status()
 
 
 def convert_to_request(f):
@@ -54,24 +38,54 @@ def convert_to_request(f):
         return method(f"{BASE_URL}/{url}", hooks=dict(response=hook), timeout=2, **parameters)
     return function
 
+def apply_options(f):
+    for k in reversed(inspect.signature(f).parameters.keys()):
+        f = options[k](f)
+    return f
 
-def convert_to_command(target):
+def convert_to_command(target,name=None):
     def inner(f):
         f = convert_to_request(f)
         f = apply_options(f)
-        return target.command(f.__name__)(f)  
+        return target.command(name or f.__name__)(f)  
     return inner
+
+def load_to_admin(*args,target):
+    def f(ctx,**kwargs):
+        sub = [x for x in args if kwargs.get(x.name)]
+        if len(sub)!=1:
+            click.echo(ctx.get_help())
+             
+            ctx.abort()
+        ctx.invoke(sub[0],**{k:v for k,v in kwargs.items() if v})
+    x = click.pass_context(f)
+    x = options["apikey"](x)
+    for arg in args:
+        for p in arg.params:
+            if p.human_readable_name=="apikey":
+                continue
+            o = optgroup.option("--"+p.human_readable_name,type=p.type,is_flag=p.is_flag)
+            x=o(x)
+            
+        
+        x = optgroup.group(f"{arg}", cls=AllOptionGroup)(x)
+    
+    return target.command("Admin")(x)
 
 
 interface = click.Group()
 
 
-@convert_to_command(interface)
-def healthcheck():
-    return requests.get, "healthcheck", {}, show_data
+
 
 
 # Data endpoints
+
+def show_data(response: requests.Response, *arg, **kwargs):
+    if response.status_code == 200:
+        click.echo(response.text)
+    else:
+        response.raise_for_status()
 
 @convert_to_command(interface)
 def SessionsPerEv(ev, datefrom, dateto, format,apikey):
@@ -96,11 +110,54 @@ def SessionsPerPoint(point, datefrom, dateto, format,apikey):
 
 @convert_to_command(interface)
 def SessionsPerProvider(provider, datefrom, dateto, format,apikey):
-    print(provider,datefrom,dateto,format,apikey)
+    
     return (
         requests.get, f"SessionsPerProvider/{provider}/{datefrom:%Y%m%d}/{dateto:%Y%m%d}", 
         {"params": dict(format=format)}, show_data
     )
+
+# authentication point
+
+def store_token(response: requests.Response, *arg, **kwargs):
+    if response.status_code == 200:
+        with open("softeng20API.token", "w") as f:
+            f.write(response.json()["token"])
+    else:
+        response.raise_for_status()
+
+@convert_to_command(interface)
+def login(username,passw):
+    return requests.post,"login",dict(data=dict(username=username,password=passw)),store_token
+
+@convert_to_command(interface)
+def logout(username,passw):
+    return requests.post,"logout",{},store_token
+
+# administrative endpoints
+
+@convert_to_command(click)
+def usermod(usermod,username,passw,apikey):
+    return requests.post,f"admin/usermod/{username}/{passw}",{},show_data
+
+@convert_to_command(click)
+def users(users,apikey):
+    return requests.post,f"admin/users/{users}",{},show_data
+
+@convert_to_command(click)
+def sessionsupd(sessionsupd,source,apikey):
+    return requests.post,f"admin/system/sessionsupd",{},show_data
+
+@convert_to_command(click)
+def healthcheck(healthcheck,apikey):
+    return requests.get, "healthcheck", {}, show_data
+
+@convert_to_command(click)
+def resetsessions(resetsessions,apikey):
+    return requests.post, "resetsessions", {}, show_data
+
+admin = load_to_admin(users,sessionsupd,resetsessions,healthcheck,usermod,target=interface)
+
+
 
 
 
