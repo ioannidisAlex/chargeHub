a38eebd69c46ea93c2a790e683cf6e97f23e6ebc
sxoliko <el16131@mail.ntua.gr>
2021-02-10 20:20:17 +0200

    validate_positive greater than zero

--- backend/common/validators.py
+++ backend/common/validators.py
@@ -1,12 +1,14 @@
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
+
 def validate_positive(value):
-    if value <= 0:
-        raise ValidationError(_("Den ftais esy , upirxe arnitiki timi se pedio pou pairnei thetikous"))
+    if value < 0:
+        raise ValidationError(_("Oups,non-positive values are not allowed"))
     return value
+
 
 def validate_language(value):
     if "fuck" in value:
-        raise ValidationError(_("Hey watch your language"))
+        raise ValidationError(_("Please be polite"))
     return value
