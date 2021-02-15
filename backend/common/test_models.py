769181907e193a78173b3dbf9b958137e934a1df
sxoliko <el16131@mail.ntua.gr>
2021-02-15 17:11:37 +0200

    test for validators

--- backend/common/test_models.py
+++ backend/common/test_models.py
@@ -1,12 +1,28 @@
 import pytest
+from django.core.exceptions import ValidationError
+from django.test import TestCase
 
-from common.models import User, VehicleOwner
-
+from common.models import Payment, User
 
 # Create your tests here.
+
+
 @pytest.mark.django_db
 def test_vehicle_owner_creation():
     user = User.objects.create(user_type=1, username="user", password="password345")
     assert user.vehicle_owner
     assert not getattr(user, "provider", None)
     assert not getattr(user, "owner", None)
+
+
+@pytest.mark.django_db
+class TestPayment(TestCase):
+    def test_validate_positive(self):
+
+        payment = Payment(
+            id=1, payment_req=False, payment_method="paypal", cost=-2, invoice="miltos"
+        )
+        with self.assertRaisesRegex(
+            ValidationError, "Oups,non-positive values are not allowed"
+        ):
+            payment.full_clean()
