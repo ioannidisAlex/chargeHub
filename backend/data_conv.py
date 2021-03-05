2ff5353b01d13f9f2c8002c675ef048f9669399d
iliaspapa <il.papandreou@gmail.com>
2021-03-05 02:44:46 +0200

    most of the data

--- backend/data_conv.py
+++ backend/data_conv.py
@@ -1,6 +1,10 @@
 import json
 import uuid
 import random
+
+def randomint(fr,to):
+    return int(random.uniform(fr,to))
+
 with open('raw datafiles/electric_vehicles_data.json') as ve:
     vehicles = json.load(ve)
 
@@ -82,30 +86,138 @@ for i in range(500):
     pay['model']="ivasimas.payments"
     filds={}
     filds['payment_req']=True
-    filds['payment_method']=_PAYMENT_METHODS[int(random.uniform(0.0,4.0))][1]
+    filds['payment_method']=_PAYMENT_METHODS[randomint(0,4)][0]
     filds['cost']=random.uniform(8.0,13.0)
     filds['invoice']=""
-    filds['user_id']='u'+str(int(random.uniform(3.0,148.0)))
+    filds['user_id']='u'+str(randomint(3,148))
     pay['fields']=filds
     payments.append(pay)
 
 with open('final data/Payment.json','w') as out1:
     json.dump(payments,out1,indent = 4)
 
+CONNECTION_TYPE_CHOICES = [
+        (7, "Avcon Connector"),
+        (4, "Blue Commando (2P+E)"),
+        (3, "BS1363 3 Pin 13 Amp"),
+        (32, "CCS (Type 1)"),
+        (33, "CCS (Type 2)"),
+        (16, "CEE 3 Pin"),
+        (17, "CEE 5 Pin"),
+        (28, "CEE 7/4 - Schuko - Type F"),
+        (23, "CEE 7/5"),
+        (18, "CEE+ 7 Pin"),
+        (2, "CHAdeMO"),
+        (13, "Europlug 2-Pin (CEE 7/16)"),
+        (1038, "GB-T AC - GB/T 20234.2 (Socket)"),
+        (1039, "GB-T AC - GB/T 20234.2 (Tethered Cable)"),
+        (1040, "GB-T DC - GB/T 20234.3"),
+        (34, "IEC 60309 3-pin"),
+        (35, "IEC 60309 5-pin"),
+        (5, "LP Inductive"),
+        (10, "NEMA 14-30"),
+        (11, "NEMA 14-50"),
+        (22, "NEMA 5-15R"),
+        (9, "NEMA 5-20R"),
+        (15, "NEMA 6-15"),
+        (14, "NEMA 6-20"),
+        (1042, "NEMA TT-30R"),
+        (36, "SCAME Type 3A (Low Power)"),
+        (26, "SCAME Type 3C (Schneider-Legrand)"),
+        (6, "SP Inductive"),
+        (1037, "T13 - SEC1011 ( Swiss domestic 3-pin ) - Type J"),
+        (30, "Tesla (Model S/X)"),
+        (8, "Tesla (Roadster)"),
+        (31, "Tesla Battery Swap"),
+        (27, "Tesla Supercharger"),
+        (1041, "Three Phase 5-Pin (AS/NZ 3123)"),
+        (1, "Type 1 (J1772)"),
+        (25, "Type 2 (Socket Only)"),
+        (1036, "Type 2 (Tethered Connector)"),
+        (29, "Type I (AS 3112)"),
+        (0, "Unknown"),
+        (24, "Wireless Charging"),
+        (21, "XLR Plug (4 pin)"),
+    ]
+
+CURRENT_TYPE_CHOICES = [
+    (1, "AC - Single Phase"),
+    (2, "AC - Three Phase"),
+    (3, "DC"),
+]
+
+STATUS_TYPE_CHOICES = [
+    (0, "Unknown"),
+    (10, "Currently Available (Automated Status)"),
+    (20, "Currently In Use (Automated Status)"),
+    (30, "Temporarily Unavailable"),
+    (50, "Operational"),
+    (75, "Partly Operational (Mixed)"),
+    (100, "Not Operational"),
+    (150, "Planned For Future Date"),
+    (200, "Removed (Decommissioned)"),
+    (210, "Removed (Duplicate Listing)"),
+]
+
+USAGE_TYPE_CHOICES = [
+    (0, "Unknown"),
+    (6, "Private - For Staff"),
+    (2, "Private - Restricted Access"),
+    (3, "Privately Owned - Notice Required"),
+    (1, "Public"),
+    (4, "Public - Membership Required"),
+    (7, "Public - Notice Required"),
+    (5, "Public - Pay At Location"),
+]
+
+CHARGER_TYPE_CHOICES = [
+    (1, "Low"),
+    (2, "Medium"),
+    (3, "High"),
+]
+
+KW_POWER_CHOICES = [
+    (1, "Under 2 kW"),
+    (2, "Over 2 kW"),
+    (3, "Over 40 kW"),
+]
+
 with open('raw datafiles/poi3.json') as po:
     points = json.load(po)
 
 charging_stations=[]
+charging_points=[]
 for i in points['data']:
     station={}
     # print(i)
     station['id']=i['UUID']
     station['model']='ivasimas.ChargingStation'
     st={}
-    st['cluster']=clusters['data'][int(random.uniform(0.0,len(clusters['data'])))]['id']
+    st['cluster']=clusters['data'][randomint(0,len(clusters['data']))]['id']
     st['owner']=str(uuid.uuid4())
     station['fields']=st
     charging_stations.append(station)
+    for co in i['Connections']:
+        c_points={}
+        c_points['id']=str(uuid.uuid4())
+        c_points['model']='ivasimas.ChargingPoint'
+        poi={}
+        poi['charging_station']=station['id']
+        poi['connection_type']=co['ConnectionType']['ID']
+        poi['current_type']=co['CurrentTypeID']
+        poi['status_type']=co['StatusTypeID']
+        poi['location']=str(uuid.uuid4())
+        poi['charger_type']=co['LevelID']
+        if i['UsageType']!=None:
+            poi['usage_type_id']=i['UsageType']['ID']
+        poi['kw_power']=co['PowerKW']
+        poi['usage_cost']=random.uniform(0.06,0.18)
+        poi['volts_power']=co['Voltage']
+        poi['amps_power']=co['Amps']
+        c_points['fields']=poi
+        charging_points.append(c_points)
 
 with open('final data/ChargingStations.json','w') as out1:
     json.dump(charging_stations,out1,indent = 4)
+with open('final data/ChargingPoints.json','w') as out1:
+    json.dump(charging_points,out1,indent = 4)
