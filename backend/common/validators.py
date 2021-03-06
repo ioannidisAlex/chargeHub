be71157fb892da0a77b55b1ea85ae0986dfd6b85
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2021-03-06 16:40:47 +0200

    performance optimization for validate doors,see #17

--- backend/common/validators.py
+++ backend/common/validators.py
@@ -1,11 +1,13 @@
 from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.utils.translation import gettext_lazy as _
 
+# def validate_positive(value):
+#     if value < 0:
+#         raise ValidationError(_("Oups,non-positive values are not allowed"))
+#     return value
 
-def validate_positive(value):
-    if value < 0:
-        raise ValidationError(_("Oups,non-positive values are not allowed"))
-    return value
+validate_positive = MinValueValidator(0)
 
 
 def validate_language(value):
