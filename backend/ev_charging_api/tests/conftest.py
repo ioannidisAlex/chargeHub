91644c82472e7532c0549cee6504d3ac7222330b
MIKE <mikegeorg98@gmail.com>
2021-03-07 18:30:36 +0200

    Some test

--- backend/ev_charging_api/tests/conftest.py
+++ backend/ev_charging_api/tests/conftest.py
@@ -4,15 +4,6 @@ from rest_framework.test import APIClient
 
 
 @pytest.fixture
-def vehicle_owner_api_client(django_user_model):
-    user = django_user_model.objects.create(username="client", password="client")
-    token = Token.objects.create(user=user)
-    client = APIClient()
-    client.credentials(HTTP_X_OBSERVATORY_AUTH="Token " + token.key)
-    # client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
-    return client
-
-@pytest.fixture
 def admin_api_client(admin_user):
     token = Token.objects.create(user=admin_user)
     client = APIClient()
@@ -47,3 +38,4 @@ def unauthenticated_api_client():
 @pytest.fixture
 def api_client(authenticated_api_client):
     return authenticated_api_client
+
