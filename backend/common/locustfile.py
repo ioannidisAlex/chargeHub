10dd9e3fc6656ebf26d305446258f9e2e4b67b40
sxoliko <el16131@mail.ntua.gr>
2021-03-07 20:34:30 +0200

    load testing

---
+++ backend/common/locustfile.py
@@ -0,0 +1,67 @@
+import time
+
+from locust import HttpUser, TaskSet, task
+
+LOGIN_URL = "/login/?next=/"
+DATA = {"username": "fedra", "password": "gggggggg8*"}
+
+"""
+class LoginAndGet(TaskSet):
+    def on_start(self):
+        self.login()
+    def login(l):
+        response = l.client.get(LOGIN_URL, name='LOGIN_URL')
+        csrftoken = response.cookies['csrftoken']
+        l.client.post(LOGIN_URL, {"username": "fedra", "password": "gggggggg8*"}, headers={"X-CSRFToken": csrftoken}, name="LOGIN_URL")
+
+    task(1)
+    def lessson(l):
+        l.client.get("http://127.0.0.1:8000/profile")
+"""
+
+
+class MyLocust(HttpUser):
+
+    min_wait = 0
+    max_wait = 0
+
+    csrftoken = "m"
+    x_observatory_auth = "m"
+
+    def on_start(self):
+        response = self.client.get("http://127.0.0.1:8000/login/?next=/")
+        global csrftoken
+        csrftoken = response.cookies["csrftoken"]
+        self.client.post(
+            "http://127.0.0.1:8000/login/",
+            data=DATA,
+            headers={"X-CSRFToken": csrftoken},
+        )
+        response2 = self.client.post(
+            "/evcharge/api/login/", data=DATA, headers={"X-CSRFToken": csrftoken}
+        )
+        global x_observatory_auth
+        # x_observatory_auth = response2.cookies['X-OBSERVATORY_AUTH']
+        x_observatory_auth = response2.text
+        x_observatory_auth = x_observatory_auth[10:-2]
+        # print(text)
+
+    # task_set = LoginAndGet
+
+    @task
+    def take_profil(self):
+        # self.client.get('/profile')
+        global csrftoken
+        specialtoken = csrftoken
+        global x_observatory_auth
+        specialauth = "Token " + x_observatory_auth
+
+        HEADERS = {"X-CSRFToken": specialtoken, "X-OBSERVATORY-AUTH": specialauth}
+
+        # id = 123e4567-e89b-12d3-a456-426614174000
+        # {"X-CSRFToken": specialtoken, "X-OBSERVATORY-AUTH": specialauth}
+        self.client.get(
+            "/evcharge/api/SessionsPerPoint/123e4567-e89b-12d3-a456-426614174000/19981111/19981212/",
+            data=DATA,
+            headers=HEADERS,
+        )
