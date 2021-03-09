68c20c60b802d0db8be7dd885f99d0eb34948b0f
Tzourhs <jimaras20111@gmail.com>
2021-03-09 15:03:13 +0200

    me sugxwreite, ksana pusharw eixa allo ena minor bug

--- front-end/common/views.py
+++ front-end/common/views.py
@@ -195,6 +195,7 @@ class SessionsPerEVView(View):
     def get(self, request, *args, **kwargs):
         return render(
             request,
+            self.template_name,
             {
                 "form": self.form_class
             },
