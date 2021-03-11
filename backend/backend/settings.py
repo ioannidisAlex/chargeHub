177a53bca14aed1fcf2d7152117a4b16fcd144e1
MIKE <mikegeorg98@gmail.com>
2021-03-11 19:28:06 +0200

    fixed deletion of token in logout and backend is now at port 8765

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -169,4 +169,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
     "x-observatory-auth",
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:8000"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000"]
