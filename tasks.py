048015608845625c77a33ced75dbc0c5ad225186
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2021-03-13 16:46:47 +0200

    ssl working and invoke first attempt

---
+++ tasks.py
@@ -0,0 +1,38 @@
+import os
+
+from invoke import task
+
+WINDOWS = os.name == "nt"
+openssl = '"C:\\Program Files\\Git\\usr\\bin\\openssl"' if WINDOWS else "openssl"
+
+
+def inside_virtual(e, *commands):
+    if WINDOWS:
+        return " & ".join(
+            [
+                f"cd {e}",
+                "call .venv\\Scripts\\activate",
+                *commands,
+                "call deactivate",
+                "cd ..",
+            ]
+        )
+
+
+@task
+def certificate(c):
+    s = f"{openssl} req -x509 -out localhost.crt -keyout localhost.key -newkey rsa:2048 -nodes -sha256  -subj '/CN=localhost' -extensions EXT -config config.txt"
+    print(s)
+    c.run(s)
+
+
+@task
+def run(c, front=False, back=False):
+    assert front or back
+    d, port = (front and ("front-end", 8000)) or (back and ("backend", 8765))
+    c.run(
+        inside_virtual(
+            d,
+            f"python manage.py runsslserver {port} --certificate ..\\localhost.crt --key ..\\localhost.key",
+        )
+    )
