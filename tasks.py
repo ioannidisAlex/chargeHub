959efa040c9a8ca685700cf570b3ad8ca24e44b1
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2021-03-14 14:51:01 +0200

    invoke clean and build

--- tasks.py
+++ tasks.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import sys
 
 from invoke import task
 
@@ -28,6 +30,31 @@ def certificate(c):
     s = f"{openssl} req -x509 -out localhost.crt -keyout localhost.key -newkey rsa:2048 -nodes -sha256  -subj '/CN=localhost' -extensions EXT -config config.txt"
     print(s)
     c.run(s)
+
+
+@task
+def clean(c, front=False, back=False, cli=False):
+    everything = not (front or back or cli)
+    for flag, folder in [(front, "front-end"), (back, "backend"), (cli, "cli-client")]:
+        if flag or everything:
+            try:
+                shutil.rmtree(os.path.join(folder, ".venv"))
+            except:
+                pass
+
+
+@task
+def build(c, front=False, back=False, cli=False):
+    everything = not (front or back or cli)
+    for flag, folder in [(front, "front-end"), (back, "backend"), (cli, "cli-client")]:
+        if flag or everything:
+            try:
+                c.run(f"{sys.executable} -m virtualenv {os.path.join(folder,'.venv')}")
+                c.run(
+                    inside_virtual(folder, "python -m pip install -r requirements.txt ")
+                )
+            except:
+                pass
 
 
 @task
